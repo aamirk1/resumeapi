d1d64fa1934c4ea4a4729de628ef8f42c54c2615
Aamir Khan <aamir.k7199@gmail.com>
2024-05-19 14:39:08 +0530

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from api import views
+from django.urls import path
+
+urlpatterns = [
+    path('resume/',views.ProfileView.as_view(), name = 'resume'),
+    path('list/',views.ProfileView.as_view(), name = 'list')
+] 
